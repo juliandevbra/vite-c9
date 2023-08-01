8b0bb91a91e40586851216afbbe232911111d60b
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2023-07-31 21:20:33 -0300

    Clase 3 - Class y function component - Props

---
+++ src/Components/ClassComponent.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+export default class ClassComponent extends Component {
+    render(){
+        return (
+            <h1>Este es mi componente de clase</h1>
+        )
+    }
+}
+
+// export class OtroComponent extends Component {
+//     render(){
+//         return (
+//             <h1>Este es otro componente</h1>
+//         )
+//     }
+// }
---
+++ src/Components/FuncComponent.jsx
@@ -0,0 +1,10 @@
+const FuncComponent = ({titulo, descripcion}) => {
+
+    return(<>
+            <h1>Bienvenidos a la sección de {titulo}</h1>
+            <p>{descripcion}</p>
+        </>
+    )
+}
+
+export default FuncComponent
