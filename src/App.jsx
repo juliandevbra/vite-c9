27b08698e1452610f887e198b0eb2976836fb509
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2023-08-02 21:12:41 -0300

    clase 4 - Estilos y map

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
+import Home from './Pages/Home'
+import Navbar from './Components/Navbar/Navbar'
 import './App.css'
-import ClassComponent from './Components/ClassComponent'
-import FuncComponent from './Components/FuncComponent'
-// import ClassComponent, {OtroComponent} from './Components/ClassComponent'  //Ejemplo de como importar con export común
 
 function App() {
 
-  let titulo1 = 'Inicio'
-  let titulo2 = 'Shop'
-
   return (
-    <>
-      <ClassComponent/>
-      {/* <OtroComponent/> */}
-      <FuncComponent titulo={titulo1} descripcion={'Esta es la descripción del inicio'}/>
-      <FuncComponent titulo={titulo2} descripcion={'Vea todas las ofertas que tenemos para ofrecerle'}/>
-    </>
+    <div className='App'>
+      <Navbar/>
+      <Home/>
+    </div>
   )
 }
 
